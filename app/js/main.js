1b2cc024247fae6c2813fd2c5fd931174223e03e
Magga375 <danila.mashkov.01@gmail.com>
2020-05-21 00:07:35 +0300

    added product-slider and  product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
-
+    var mixer = mixitup('.products__inner-box');
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+    $('.products-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 });
